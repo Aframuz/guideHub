240a43e402489576e8487a93ec30fed012adcbde
Aframuz <aframuz@tuta.io>
2023-03-11 13:17:33 -0300

    simple branch created

--- routes/guide-route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const guideController = require('../controllers/guide-controller')
-
-const router = express.Router()
-
-router.route('/').get(guideController.getGuides)
-
-module.exports = router
